38823c9e1c673a326f8783f57f75ec0963715fd2
JoanDoe11 <33231314+JoanDoe11@users.noreply.github.com>
2020-03-29 16:21:14 +0200

    add file reading

--- twitter_sentiment.py
+++ twitter_sentiment.py
@@ -1,7 +1,17 @@
-def load_training_set():
+import csv
+
+def load_training_set(path):
+
+    # rt = read as text
+    with open(path,'rt', encoding = "ISO-8859-1") as datafile:
+        reader = csv.reader(datafile)
+        header = next(reader)
+
     return ""
 
 if __name__ == '__main__':
-    print("Loading training data...")
+    print("Enter path to csv file:")
+    path = input()
 
-    training_data = load_training_set()
+    print("Loading training data...")
+    training_data = load_training_set(path)
